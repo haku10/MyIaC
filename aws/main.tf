cd061e6549ca21adac76254b72477562333022a8
haku10 <isptry5689@gmail.com>
2020-11-22 23:20:12 +0900

    vpc&EC2インスタンス関連変更

--- aws/main.tf
+++ aws/main.tf
@@ -1,8 +1,76 @@
-resource "aws_instance" "my_instance1" {
+# EC2 instance
+resource "aws_instance" "my-instance1" {
   ami           = var.ami_id
   instance_type = var.aws_instance_type
-  subnet_id = aws_subnet.my_subnet1_1.id
+  subnet_id = aws_subnet.my-subnet1-1.id
   tags = {
     Name = var.aws_instance_tag_name
+  }
+}
+
+#Elastic ip
+resource "aws_eip" "my-instance1-eip" {
+  instance = aws_instance.my-instance1.ip
+  vpc      = true
+}
+
+# EC2 load_balancer
+resource "aws_alb" "my-alb" {
+  name            = "${var.stage}-my-alb"
+  internal        = false
+  security_groups = aws_security_group.alb.id
+  subnets         = aws_subnet.my_subnet1_1.id
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = var.stage
+  }
+  access_logs {
+    bucket  = module.s3.alb-logs.bucket
+    prefix  = "test-${var.stage}-alb"
+    enabled = true
+  }
+}
+
+# EC2 target_group
+resource "aws_alb_target_group" "my-target-group1" {
+  name                 = "${var.stage}-my-app"
+  port                 = 80
+  protocol             = "HTTP"
+  target_type          = "ip"
+  vpc_id               = aws_vpc.my-vpc1.id
+  deregistration_delay = 30
+
+  health_check {
+    interval            = 60
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 20
+    unhealthy_threshold = 10
+    matcher             = 200
+  }
+}
+
+# target_group_attachment
+resource "aws_alb_target_group_attachment" "alb" {
+  count            = 2
+  target_group_arn = "${element(aws_alb_target_group.my-target-group1.*.arn, count.index)}"
+  target_id        = "${element(aws_instance.my-instance1.id, count.index)}"
+  port             = 80
+}
+
+# EC2 alb_listener
+resource "aws_alb_listener" "my-alb-listener" {
+  load_balancer_arn = aws_alb.test.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+  certificate_arn   = aws_acm_certificate.test.arn
+
+  default_action {
+    target_group_arn = aws_alb_target_group.test-app.arn
+    type             = "forward"
   }
 }
--- aws/vpc.tf
+++ aws/vpc.tf
@@ -1,18 +1,68 @@
-# Create a VPC
-resource "aws_vpc" "my_vpc1" {
+# VPC
+resource "aws_vpc" "my-vpc1" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
   enable_dns_support = "true"
   enable_dns_hostnames = "false"
 }
 
-# Create a subnet
-resource "aws_subnet" "my_subnet1_1" {
+# subnet1
+resource "aws_subnet" "my-subnet1-1" {
     vpc_id = aws_vpc.my_vpc1.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "ap-northeast-1a"
 
   tags = {
-    Name = "my_subnet1_1"
+    Name = "my-subnet1-1"
+  }
+}
+
+# subnet2
+resource "aws_subnet" "my-subnet1-2" {
+    vpc_id = aws_vpc.my_vpc1.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "my-subnet1-2"
+  }
+}
+
+# subnet3
+resource "aws_subnet" "my-subnet1-3" {
+    vpc_id = aws_vpc.my-vpc1.id
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "ap-northeast-1d"
+
+  tags = {
+    Name = "my-subnet1-3"
+  }
+}
+
+# network_acl
+resource "aws_network_acl" "my-network-acl" {
+  vpc_id = aws_vpc.my-vpc1.id
+  subnet_ids  = ["${aws_subnet.my-subnet1-1.id}", "${aws_subnet.my-subnet1-2.id}", "${aws_subnet.my-subnet1-3.id}"]
+  
+  ingress {
+  protocol   = "all"
+  rule_no    = 100
+  action     = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port  = 0
+  to_port    = 0
+  }
+
+  egress {
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "my-network-acl"
   }
 }
